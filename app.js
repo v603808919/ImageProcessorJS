f8a2085a68157fbc8ed7a7032a20b09b6d0a8813
Server IG C10 <support@igecsystems.com>
2018-04-09 12:58:07 +0300

    function for one IMG

--- app.js
+++ app.js
@@ -1,57 +1,26 @@
 
-'use strict';
+"use strict";
 
-let fs = require('fs');
-let watermark = require('image-watermark');
-let dir = require('node-dir');
-let path = require('path');
-let crypto = require('crypto');
-let async = require('async');
+let fs = require("fs");
+let watermark = require("image-watermark");
+//let path = require("path");
+let crypto = require("crypto");
+//let async = require("async");
 
 
-//console.log('Start update img');
+//const maxOneTimeRequest = 50;
 
 const regexpImg = /\.(?:jp(?:e?g|e|2)|png)$/; // Регулярное выражение для отбора файлов изображений 
 
 let options = {
-  'text': 'hvac-store.com',
-  'override-image': true
+  "text": "hvac-store.com",
+  "override-image": true
 };
 
 
 
-//let imgFilePath = __dirname + '/demo/api.png';
-let imgFiledir = '/modulesjs/ImageProcessorJS/demo/';
-
-
-
-
-//Промис функция расчета Хеш (пока не используется)
-function fileHash(filename, algorithm = 'md5') {
-  return new Promise((resolve, reject) => {
-    // Algorithm depends on availability of OpenSSL on platform
-    // Another algorithms: 'sha1', 'md5', 'sha256', 'sha512' ...
-    let shasum = crypto.createHash(algorithm);
-    try {
-      let s = fs.ReadStream(filename)
-      s.on('data', function (data) {
-        shasum.update(data)
-      })
-      // making digest
-      s.on('end', function () {
-        const hash = shasum.digest('hex')
-        return resolve(hash);
-      })
-    } catch (error) {
-      return reject('calc fail');
-    }
-  });
-}
-
-
-
-
-
+//let imgFilePath = __dirname + "/demo/api.png";
+let imgFiledir = "/modulesjs/ImageProcessorJS/demo/";
 
 // Спсисок файлов дирректории
 const files = fs.readdirSync(imgFiledir).reduce((p, c) => {
@@ -63,6 +32,7 @@ const files = fs.readdirSync(imgFiledir).reduce((p, c) => {
   return p;
 }, []);
 
+
 console.log(files);
 
 
@@ -71,10 +41,10 @@ console.log(files);
 files.forEach(function (item, i, arr) {
   
   let imgFilePath = imgFiledir + item;
-  console.log('forEach - ' + imgFilePath);
+  console.log("forEach - " + imgFilePath);
   if (!(checkHashfile(imgFilePath)) ) {
     // Ставим водный знак на конкретный файл
-    console.log('start  watermark - ' + imgFilePath);
+    console.log("start  watermark - " + imgFilePath);
     watermark.addWatermark(imgFilePath, options, function (err) {
       if (err)
         return console.log(err);
@@ -86,74 +56,127 @@ files.forEach(function (item, i, arr) {
 }); */
 
 
-// Перебираем все отобранные файлы
-files.forEach(function (item, i, arr) {
-  
-  let imgFilePath = imgFiledir + item;
-  console.log('forEach - ' + imgFilePath);
 
-  checkHashfile(imgFilePath, function(check) {
-    if (!check) {
-      // Ставим водный знак на конкретный файл
-      console.log('start  watermark - ' + imgFilePath);
-      watermark.embedWatermarkWithCb(imgFilePath, options, function (err) {
-        if (err)
-          return console.log(err);
-        if (!err)
-          makeHashfile(imgFilePath); //Создаем Hash файл
-        return console.log("Successful - no error " + item);
-      });
+const fileslength = files.length; //Запоминаем длинну массива 
+console.log("fileslength: " + fileslength);
+/*
+for (let i = 0; i < fileslength; i = i + maxOneTimeRequest) {
+
+  let maxj = i + maxOneTimeRequest; // вычисляем границы
+  if (maxj > fileslength) {
+    maxj = fileslength;
+  }
+
+  for (let j = i; j < maxj; j++) {
+    console.log(j);
+    
+    let imgFilePath = imgFiledir + files[j];
+    console.log("forEach - " + imgFilePath);
+  
+    checkHashfile(imgFilePath, function(check) {
+      if (!check) {
+        // Ставим водный знак на конкретный файл
+        console.log("start  watermark - " + imgFilePath);
+        watermark.embedWatermarkWithCb(imgFilePath, options, function (err) {
+          if (err)
+            return console.log(err);
+          if (!err)
+            makeHashfile(imgFilePath); //Создаем Hash файл
+          return console.log("Successful - no error " + files[j]);
+        });
+      }
+  
+    }) ;
+
+  }
+
+}*/
+
+// Запускаем перебр послеловательно
+let imgFilePathFirst = imgFiledir + files[0];
+var imgNum = 0;
+
+checkHashfile(imgFilePathFirst, (err, data) => {
+  console.log("start callback imgFilePathFirst " + imgNum + ". data: " + data);
+  let imgFilePath = imgFiledir + files[imgNum];
+  imgNum++;
+
+  if (!data) {
+    // Ставим водный знак на конкретный файл
+    console.log("start  watermark - " + files[imgNum] );
+    watermark.embedWatermarkWithCb(imgFilePath, options, function (err) {
+      if (imgNum < fileslength) {  // Если массив еще не кончился
+        console.log("Start next loop - " + imgNum);
+        //checkHashfile(imgFiledir + files[imgNum]); //коментируем закольцовывание
+      }
+      if (err)
+        return console.log(err);
+      if (!err)
+        makeHashfile(imgFilePath); //Создаем Hash файл
+      return console.log("Successful - no error " + imgFilePath);
+    });
+  } else { // 
+    if (imgNum < fileslength) {  // Если массив еще не кончился
+      console.log("WOVM - Start next loop - " + imgNum);
+      //checkHashfile(imgFiledir + files[imgNum]); //коментируем закольцовывание
     }
 
-  }) ;
+  }
+
+}) ;
+
+
+/* 
+// тест 
+checkHashfile(imgFilePathFirst, (err, data) => {
+  console.log("start callback err " + err);
+  console.log("start callback data " + data);
 });
-
-
-
+ */
 
 
 // проверяем соовтествует ли Hash имеющемуся файлу - boolean
 // false - или Шеша нет или он не соответствует
 // true - с изобрадением все ок, оно промаркировано.
 function checkHashfile(img, callback) {
-  const HashFileName = img + '.hash';
+  const HashFileName = img + ".hash";
 
   const imgfd = fs.createReadStream(img);
   const imghash = crypto.createHash("md5");
   imghash.setEncoding("hex");
 
-  //console.log(imghash);
+
 
   //Проверка существования файла Hash
   fs.readFile(HashFileName, (err, data) => {
     if (err) {
-      console.log('no file ' + HashFileName)
+      console.log("no file " + HashFileName);
       //return false;
       //throw (err);
-      callback(false);
+      return callback(err, false);
     } else {
-      console.log('file have ' + HashFileName)
-      const HashFile = data.toString('utf8');
+      console.log("file have " + HashFileName);
+      const HashFile = data.toString("utf8");
 
-      imgfd.on('data', function (data) {
-        imghash.update(data)
-      })
+      imgfd.on("data", function (data) {
+        imghash.update(data);
+      });
       // making digest
-      imgfd.on('end', function () {
-        const hashimg = imghash.digest('hex');
+      imgfd.on("end", function () {
+        const hashimg = imghash.digest("hex");
         console.log(hashimg);
         console.log(HashFile);
         if (hashimg === HashFile) {
-          console.log('HashFileName - true ' + img);
+          console.log("HashFileName - true " + img);
           //return true;
-          callback(true);
+          return callback(null, true);
         } else {
-          console.log('HashFileName - false ' + img);
+          console.log("HashFileName - false " + img);
           //return false
-          callback(false);
+          return callback(null,false);
         }
 
-      })
+      });
 
       //fd.pipe(hash);
 
@@ -162,8 +185,11 @@ function checkHashfile(img, callback) {
   });
 
 
+  console.log("Finish checkHashfile " + HashFileName);
 
-};
+
+
+}
 
 
 
@@ -171,27 +197,23 @@ function checkHashfile(img, callback) {
 
 // Создаем Hash файл
 function makeHashfile(img) {
-  console.log('start make Hashfile ' + img )
+  console.log("start make Hashfile " + img);
   const fd = fs.createReadStream(img);
   const hash = crypto.createHash("md5");
   hash.setEncoding("hex");
   fd.on("end", () => {
     hash.end();
-    let HashFileName = img + '.hash';
+    let HashFileName = img + ".hash";
     console.log(HashFileName);
     //fs.appendFileSync(HashFileName, `${hash.read()}`);
     fs.writeFileSync(HashFileName, `${hash.read()}`);
   });
 
   fd.pipe(hash);
-};
+}
 
 
 
-
-
-console.log('Finish update img');
-
-
+console.log("Finish update img");
 
 
